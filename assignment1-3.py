a7947493fb064b0e99b22e9c7528322b589c4edb
jaketyjake <jaketyjake@gmail.com>
2018-05-04 13:25:35 -0500

    rewrote smallest and largest programs to sort without built in functions

--- assignment1-3.py
+++ assignment1-3.py
@@ -1,7 +1,17 @@
 #find the smallest element in array
-array= [324523452352345,12, 123,1234,12345,123456,1234567]
-order = sorted(array)
-print(order[0])
+#array= [324523452352345,12, 123,1234,12345,123456,1234567]
+#order = sorted(array)
+#print(order[0])
 
-
+#rewrote without using any built in functions to sort
+array = [324523452352345,12, 123,1234,12345,123456,1234567]
+new_array =[]
+while array:
+    lowest = array[0]
+    for number in array:
+        if number < lowest:
+            lowest = number
+    new_array.append(lowest)
+    array.remove(lowest)        
+print(new_array[0])
 
